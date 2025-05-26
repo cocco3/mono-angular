e6632364cf34280a5d729c7cc573338164c96684
Joe Cocco <cocco3@gmail.com>
2025-05-25 20:31:12 -0700

    feat: Create app vampire survivors evolutions (#19)

---
+++ projects/vampire-survivors/src/data/passives.ts
@@ -0,0 +1,162 @@
+import type { Item } from './types';
+
+export const HollowHeart: Item = {
+  kind: 'passive',
+  name: 'Hollow Heart',
+  image: 'hollow-heart.png',
+  wiki: 'https://vampire.survivors.wiki/w/Hollow_Heart',
+};
+
+export const EmptyTome: Item = {
+  kind: 'passive',
+  name: 'Empty Tome',
+  image: 'empty-tome.png',
+  wiki: 'https://vampire.survivors.wiki/w/Empty_Tome',
+};
+
+export const Bracer: Item = {
+  kind: 'passive',
+  name: 'Bracer',
+  image: 'bracer.png',
+  wiki: 'https://vampire.survivors.wiki/w/Bracer',
+};
+
+export const Candelabrador: Item = {
+  kind: 'passive',
+  name: 'Candelabrador',
+  image: 'candelabrador.png',
+  wiki: 'https://vampire.survivors.wiki/w/Candelabrador',
+};
+
+export const Clover: Item = {
+  kind: 'passive',
+  name: 'Clover',
+  image: 'clover.png',
+  wiki: 'https://vampire.survivors.wiki/w/Clover',
+};
+
+export const Spellbinder: Item = {
+  kind: 'passive',
+  name: 'Spellbinder',
+  image: 'spellbinder.png',
+  wiki: 'https://vampire.survivors.wiki/w/Spellbinder',
+};
+
+export const Spinach: Item = {
+  kind: 'passive',
+  name: 'Spinach',
+  image: 'spinach.png',
+  wiki: 'https://vampire.survivors.wiki/w/Spinach',
+};
+
+export const Pummarola: Item = {
+  kind: 'passive',
+  name: 'Pummarola',
+  image: 'pummarola.png',
+  wiki: 'https://vampire.survivors.wiki/w/Pummarola',
+};
+
+export const Attractorb: Item = {
+  kind: 'passive',
+  name: 'Attractorb',
+  image: 'attractorb.png',
+  wiki: 'https://vampire.survivors.wiki/w/Attractorb',
+};
+
+export const Armor: Item = {
+  kind: 'passive',
+  name: 'Armor',
+  image: 'armor.png',
+  wiki: 'https://vampire.survivors.wiki/w/Armor_(item)',
+};
+
+export const Duplicator: Item = {
+  kind: 'passive',
+  name: 'Duplicator',
+  image: 'duplicator.png',
+  wiki: 'https://vampire.survivors.wiki/w/Duplicator',
+};
+
+export const Crown: Item = {
+  kind: 'passive',
+  name: 'Crown',
+  image: 'crown.png',
+  wiki: 'https://vampire.survivors.wiki/w/Crown',
+};
+
+export const StoneMask: Item = {
+  kind: 'passive',
+  name: 'Stone Mask',
+  image: 'stone-mask.png',
+  wiki: 'https://vampire.survivors.wiki/w/Stone_Mask',
+};
+
+export const Skull_O_Maniac: Item = {
+  kind: 'passive',
+  name: `Skull O'Maniac`,
+  image: 'skull-o-maniac.png',
+  wiki: `https://vampire.survivors.wiki/w/Skull_O'Maniac`,
+};
+
+export const Wings: Item = {
+  kind: 'passive',
+  name: 'Wings',
+  image: 'wings.png',
+  wiki: 'https://vampire.survivors.wiki/w/Wings',
+};
+
+export const TorronasBox: Item = {
+  kind: 'passive',
+  name: `Torrona's Box`,
+  image: 'torronas-box.png',
+  wiki: `https://vampire.survivors.wiki/w/Torrona's_Box`,
+};
+
+export const Tirajisu: Item = {
+  kind: 'passive',
+  name: 'Tirajisú',
+  image: 'tirajisu.png',
+  wiki: 'https://vampire.survivors.wiki/w/Tirajis%C3%BA',
+};
+
+export const GoldRing: Item = {
+  kind: 'passive',
+  name: 'Gold Ring',
+  image: 'gold-ring.png',
+  wiki: 'https://vampire.survivors.wiki/w/Gold_Ring',
+};
+
+export const SilverRing: Item = {
+  kind: 'passive',
+  name: 'Silver Ring',
+  image: 'silver-ring.png',
+  wiki: 'https://vampire.survivors.wiki/w/Silver_Ring',
+};
+
+export const MetaglioLeft: Item = {
+  kind: 'passive',
+  name: 'Metaglio Left',
+  image: 'metaglio-left.png',
+  wiki: 'https://vampire.survivors.wiki/w/Metaglio_Left',
+};
+
+export const MetaglioRight: Item = {
+  kind: 'passive',
+  name: 'Metaglio Right',
+  image: 'metaglio-right.png',
+  wiki: 'https://vampire.survivors.wiki/w/Metaglio_Right',
+};
+
+export const KaromasMana: Item = {
+  kind: 'passive',
+  name: `Karoma's Mana`,
+  image: 'karomas-mana.png',
+  wiki: `https://vampire.survivors.wiki/w/Karoma's_Mana`,
+};
+
+export const ParmAegis: Item = {
+  kind: 'passive',
+  name: 'Parm Aegis',
+  image: 'parm-aegis.png',
+  wiki: 'https://vampire.survivors.wiki/w/Parm_Aegis',
+};
---
+++ projects/vampire-survivors/src/data/relics.ts
@@ -0,0 +1,22 @@
+import type { Item } from './types';
+
+export const ChaosMalachite: Item = {
+  kind: 'relic',
+  name: 'Chaos Malachite',
+  image: 'chaos-malachite.png',
+  wiki: 'https://vampire.survivors.wiki/w/Chaos_Malachite',
+};
+
+export const ChaosRosalia: Item = {
+  kind: 'relic',
+  name: 'Chaos Rosalia',
+  image: 'chaos-rosalia.png',
+  wiki: 'https://vampire.survivors.wiki/w/Chaos_Rosalia',
+};
+
+export const ChaosAltemanna: Item = {
+  kind: 'relic',
+  name: 'Chaos Altemanna',
+  image: 'chaos-altemanna.png',
+  wiki: 'https://vampire.survivors.wiki/w/Chaos_Altemanna',
+};
