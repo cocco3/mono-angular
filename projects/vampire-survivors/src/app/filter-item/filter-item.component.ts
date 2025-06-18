afa2bb53b880db3b2523aac13f80abb250063e74
Joe Cocco <cocco3@gmail.com>
2025-06-18 12:15:27 -0700

    item filters

---
+++ projects/vampire-survivors/src/app/filter-item/filter-item.component.ts
@@ -0,0 +1,23 @@
+import { booleanAttribute, Component, input, output } from '@angular/core';
+import { UiTooltipComponent, UiTooltipDirective } from '@cocco3/angular-ui';
+
+@Component({
+  imports: [UiTooltipComponent, UiTooltipDirective],
+  selector: 'app-filter-item',
+  styleUrl: './filter-item.css',
+  templateUrl: './filter-item.html',
+})
+export class FilterItemComponent {
+  checked = input(false, { transform: booleanAttribute });
+  group = input.required<string>();
+  image = input.required<string>();
+  name = input.required<string>();
+  value = input.required<string>();
+
+  readonly changed = output<{ checked: boolean; value: string }>();
+
+  protected handleChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.changed.emit({ checked: target.checked, value: target.value });
+  }
+}
