2ac0324e5b241600b376e2b11fee3ce2f184033e
Joe Cocco <cocco3@gmail.com>
2025-09-05 12:03:11 -0700

    chore: Remove invoice project (#87)

--- projects/utils/src/lib/currency.ts
+++ projects/utils/src/lib/currency.ts
@@ -1,17 +1,3 @@
-import { ToWords } from 'to-words';
-
-export const amountToWords = (amount = 0) => {
-  const toWords = new ToWords({
-    localeCode: 'en-US',
-    converterOptions: {
-      currency: true,
-      doNotAddOnly: true,
-    },
-  });
-
-  return toWords.convert(amount);
-};
-
 export const formatCurrency = (amount: number) => {
   const formatter = new Intl.NumberFormat('en-US', {
     currency: 'USD',
