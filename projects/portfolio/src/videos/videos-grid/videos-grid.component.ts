7cd0d95706245ef46f743e4980b9fa8936f75aff
Joe Cocco <cocco3@gmail.com>
2025-10-08 21:38:20 -0700

    lite youtube

--- projects/portfolio/src/videos/videos-grid/videos-grid.component.ts
+++ projects/portfolio/src/videos/videos-grid/videos-grid.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { UiItemGridComponent, SafeResourceUrlPipe } from '@cocco3/angular-ui';
+import {
+  UiItemGridComponent,
+  UiYouTubeVideoComponent,
+} from '@cocco3/angular-ui';
 import { ContentService } from '../../content/ContentService';
 
 @Component({
-  imports: [UiItemGridComponent, SafeResourceUrlPipe],
+  imports: [UiItemGridComponent, UiYouTubeVideoComponent],
   selector: 'app-videos-grid',
-  styleUrl: './videos-grid.css',
   templateUrl: './videos-grid.html',
 })
 export class AppVideosGrid {
---
+++ projects/ui/src/lib/media/youtube-video/youtube-video.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  computed,
+  CUSTOM_ELEMENTS_SCHEMA,
+  input,
+} from '@angular/core';
+import '@justinribeiro/lite-youtube';
+
+/**
+ * A lightweight YouTube embed component using the `lite-youtube` web component.
+ *
+ * Displays a thumbnail that loads the YouTube video on click,
+ * reducing page load compared to iframes.
+ *
+ * @link https://github.com/justinribeiro/lite-youtube?tab=readme-ov-file
+ */
+@Component({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  selector: 'ui-youtube-video',
+  styleUrl: './youtube-video.css',
+  templateUrl: './youtube-video.html',
+})
+export class UiYouTubeVideoComponent {
+  videoId = input.required<string>();
+  videoTitle = input.required<string>();
+
+  protected videoUrl = computed(
+    () => `https://www.youtube.com/embed/${this.videoId()}`
+  );
+  protected showTitle = true;
+
+  protected handleClick() {
+    this.showTitle = false;
+  }
+}
