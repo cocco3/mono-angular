145efd02bb69a27e5bf69dbc8c1ed77bff23d95d
Joe Cocco <cocco3@gmail.com>
2025-10-12 17:35:32 -0700

    countdown - lazy load routes

--- projects/countdown/src/app/app.routes.ts
+++ projects/countdown/src/app/app.routes.ts
@@ -1,19 +1,34 @@
 import { type Routes } from '@angular/router';
 import { AuthGuardService } from './services/AuthGuardService';
-import { PageLoginComponent } from './page-login/page-login.component';
-import { AppMainComponent } from './app-main/app-main.component';
-import { PageEventsComponent } from './events/page-events/page-events.component';
-import { PageSettingsComponent } from './settings/page-settings/page-settings.component';
 
 export const routes: Routes = [
-  { path: 'login', component: PageLoginComponent },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./page-login/page-login.component').then(
+        (m) => m.PageLoginComponent
+      ),
+  },
   {
     path: '',
-    component: AppMainComponent,
+    loadComponent: () =>
+      import('./app-main/app-main.component').then((m) => m.AppMainComponent),
     canActivate: [AuthGuardService],
     children: [
-      { path: 'events', component: PageEventsComponent },
-      { path: 'settings', component: PageSettingsComponent },
+      {
+        path: 'events',
+        loadComponent: () =>
+          import('./events/page-events/page-events.component').then(
+            (m) => m.PageEventsComponent
+          ),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./settings/page-settings/page-settings.component').then(
+            (m) => m.PageSettingsComponent
+          ),
+      },
       { path: '', redirectTo: 'events', pathMatch: 'full' },
     ],
   },
