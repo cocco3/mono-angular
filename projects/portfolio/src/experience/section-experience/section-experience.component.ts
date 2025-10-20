eac2a1761ffc47d2f81af2d5b82e66e3b25cd623
Joe Cocco <cocco3@gmail.com>
2025-10-20 01:33:07 -0700

    resume

--- projects/portfolio/src/experience/section-experience/section-experience.component.ts
+++ projects/portfolio/src/experience/section-experience/section-experience.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {
+  UiButtonComponent,
+  UiIconComponent,
+  UiSlotDirective,
+} from '@cocco3/angular-ui';
 import { AppPageSection } from '../../components/page-section/page-section.component';
 import { AppExperienceList } from '../experience-list/experience-list.component';
 
 @Component({
-  imports: [AppPageSection, AppExperienceList],
+  imports: [
+    AppPageSection,
+    AppExperienceList,
+    UiButtonComponent,
+    UiIconComponent,
+    UiSlotDirective,
+  ],
   selector: 'app-section-experience',
   templateUrl: './section-experience.html',
 })
