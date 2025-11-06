f35d918dbded59e7c7515390651a7b9e9cc7abfc
Joe Cocco <cocco3@gmail.com>
2025-11-06 13:48:35 -0800

    provideEnvironmentInitializer

--- projects/ui/src/lib/providers/analytics/analytics.provider.ts
+++ projects/ui/src/lib/providers/analytics/analytics.provider.ts
@@ -1,4 +1,10 @@
-import { makeEnvironmentProviders } from '@angular/core';
+import {
+  EnvironmentInjector,
+  inject,
+  makeEnvironmentProviders,
+  provideEnvironmentInitializer,
+} from '@angular/core';
+
 import {
   Analytics,
   type AnalyticsConfig,
@@ -13,5 +19,14 @@ export function provideAnalytics<
       provide: Analytics,
       useFactory: () => new Analytics<TTrackEvent>(config),
     },
+
+    /**
+     * Ensures instantiation of a service that performs side effects.
+     * Necessary if the service is not injected elsewhere in the app.
+     */
+    provideEnvironmentInitializer(() => {
+      const injector = inject(EnvironmentInjector);
+      injector.get(Analytics);
+    }),
   ]);
 }
