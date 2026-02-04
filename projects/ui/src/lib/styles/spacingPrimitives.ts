1909c67fb8fd1f83d5e1db39b5a051b26944679c
Joe Cocco <cocco3@gmail.com>
2026-02-03 23:25:41 -0800

    spacing

---
+++ projects/ui/src/lib/styles/spacingPrimitives.ts
@@ -0,0 +1,21 @@
+export const spacingPrimitives = {
+  base: {
+    name: 'Spacing',
+    id: 'spacing',
+    values: {
+      '25': '1px',
+      '50': '2px',
+      '100': '4px',
+      '150': '6px',
+      '200': '8px',
+      '300': '12px',
+      '400': '16px',
+      '500': '20px',
+      '600': '24px',
+      '700': '32px',
+      '800': '40px',
+      '900': '48px',
+      '1000': '64px',
+    },
+  },
+};
